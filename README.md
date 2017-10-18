e870e09314c87bb86b54518609ee6f1590649fd7
Nicolás Estrada <nicolas@skout.com>
2017-10-18 00:54:44 -0700

    PAWK-2 User login and chat retrieval, TODO: chat update

--- README.md
+++ README.md
@@ -48,3 +48,12 @@ For local testing, you need to add the following entry in `/etc/hosts`:
 ```bash
 127.0.0.1       localhost kafka
 ```
+
+### Extra Info
+
+https://wurstmeister.github.io/kafka-docker/
+https://docs.docker.com/compose/gettingstarted/#step-4-build-and-run-your-app-with-compose
+https://stackoverflow.com/questions/35861501/kafka-in-docker-not-working
+https://hub.docker.com/r/wurstmeister/kafka/
+https://github.com/PrettyPrinted/flask-socketio-chat/blob/master/main.py
+https://github.com/miguelgrinberg/Flask-SocketIO-Chat/blob/master/app/main/events.py
--- app.py
+++ app.py
@@ -1,19 +1,94 @@
-import sys
+import json
+
+import arrow
+from kafka import KafkaProducer, KafkaConsumer, TopicPartition
+from flask import Flask, render_template, request
+from forms import SignupForm, ChatForm
+
+producer = KafkaProducer(
+        bootstrap_servers=['kafka:9092'],
+        value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
 
 
-def main():
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-    while True:
-        pass
 
+@app.route("/chat/send/<user>", methods=['POST'])
+def chat_send(user):
+    form = ChatForm()
+    future = producer.send(
+        'pawk',
+        {
+            "username": user if user is not None else form.user.data,
+            "timestamp": arrow.utcnow().float_timestamp,
+            "content": form.message.data
+        })
+    _ = future.get(timeout=1)
+
+    return get(form.user.data)
+
+
+@app.route("/send/<user>/<message>", methods=['GET'])
+def send(user, message):
+    print(request.form)
+    future = producer.send(
+        'pawk',
+        {
+            "username": user,
+            "timestamp": arrow.utcnow().float_timestamp,
+            "content": message
+        })
+    _ = future.get(timeout=10)
+
+    return render_template("index.html")
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+
+    if request.method == 'POST':
+        send(form.user.data, 'JOINED THE ROOM')
+        return get(form.user.data)
+
+    elif request.method == 'GET':
+        return render_template('signup.html', form=form)
+
+
+@app.route('/get', methods=['GET'])
+def get(user=None):
+    form = ChatForm()
+
+    if user is None:
+        return signup()
+
+    chats = []
+    topic = 'pawk'
+    consumer = KafkaConsumer(
+        bootstrap_servers=['kafka:9092'],
+        value_deserializer=json.loads)
+
+    ps = [TopicPartition(topic, p) for p in consumer.partitions_for_topic(topic)]
+
+    consumer.assign(ps)
+    a = consumer.end_offsets(ps)
+    offset = a[ps[0]]
+
+    consumer.seek_to_beginning()
+
+    for msg in consumer:
+        msg.value['datetime'] = arrow.get(msg.value['timestamp']).humanize()
+        chats.append(msg.value)
+        if msg.offset >= offset - 1:
+            consumer.close()
+            break
+
+    return render_template('chats.html', chats=chats, form=form, user=user)
 
 if __name__ == '__main__':
-
-    try:
-        main()
-    except KeyboardInterrupt:
-        print('Closing app ...')
-        sys.exit(0)
-    except Exception as e:
-        print('Unknown exception: {}'.format(e))
-        sys.exit(2)
+    app.run('0.0.0.0', debug=True)
